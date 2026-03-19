9f0fe8ebdb0bbbfb576119c63ed3978074bd1f1c
rahul <rahulwagadre2000@gmail.com>
2026-03-19 18:02:05 +0530

    Modified AppRoutes import

--- frontend/smart-clinic-ui/src/routes/AppRoutes.jsx
+++ frontend/smart-clinic-ui/src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignupPage from "../pages/SIgnupPage";
+import SignupPage from "../pages/SignupPage";
 import LoginPage from "../pages/LoginPage";
 import NotFoundPage from "../pages/NotFoundPage";
 
